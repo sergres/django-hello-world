60420ee56c025f4c4c143403f1a41372e0c75b4e
sergres <sergres@gmail.com>
2012-09-12 03:23:07 +0300

    model and initial data changes
    UserProfile - class, which will store data for user addition field.
    profile is linked to User OneToOne
    
    initial_data updated and extended by UserProfile data too

--- django_hello_world/hello/models.py
+++ django_hello_world/hello/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    # This field is required.
+    user = models.OneToOneField(User)
+
+    # Other fields here
+    date_birth = models.DateField()
+    bio = models.TextField()
+    jabber =  models.EmailField()
+    skype = models.CharField(max_length=30)
+    other_contacts = models.TextField()
