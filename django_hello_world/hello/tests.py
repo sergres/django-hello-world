3f8c5d167e0421c112aa20b0f57af5e942d7278d
sergres <sergres@gmail.com>
2012-09-12 12:18:09 +0300

    created tests for userProfile and for html part

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -8,6 +8,8 @@ Replace this with more appropriate tests for your application.
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
+from django.contrib.auth.models import User
+
 
 
 
@@ -18,6 +20,16 @@ class SimpleTest(TestCase):
         """
         self.assertEqual(1 + 1, 2)
 
+    def test_userprofile(self):
+        users = User.objects.filter()
+        for user in users:
+            profile = user.get_profile()
+            profile.date_birth
+            profile.jabber
+            profile.skype
+            profile.bio
+            profile.other_contacts
+
 
 class HttpTest(TestCase):
     def test_home(self):
@@ -25,3 +37,10 @@ class HttpTest(TestCase):
         response = c.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Hello!')
+        self.assertContains(response, 'Sergiy')
+        self.assertContains(response, 'Shchypa')
+        self.assertContains(response, 'July')
+        self.assertContains(response, 'some bio text')
+        self.assertContains(response, 'admin@example.com')
+        self.assertContains(response, 'sergres@ubuntu-jabber.de')
+        self.assertContains(response, '9543171')
