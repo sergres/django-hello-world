24f77083885efbeb857fe9b68ed3eb434859bf23
sergres <sergres@gmail.com>
2012-09-12 17:37:42 +0300

    forgot to commit thèse files in previous commit
    added handler to url.py
    and implemented handler

--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -1,8 +1,16 @@
 from annoying.decorators import render_to
 from django.contrib.auth.models import User
+from django_hello_world.hello.models import RequestsStorage
+
 
 
 @render_to('hello/home.html')
 def home(request):
     users = User.objects.filter()
     return {'users': users}
+
+@render_to('hello/requestslog.html')
+def requestslog(request):
+    requests = RequestsStorage.objects.order_by("-time").filter()[:10]
+    return {'requests': requests}
+
