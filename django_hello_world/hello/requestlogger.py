a69689e6217ef662e5a2bb1f7359a12cc4d81da8
sergres <sergres@gmail.com>
2012-09-12 17:10:03 +0300

    ticket:3
    implemented RequestLogger.process_request

--- django_hello_world/hello/requestlogger.py
+++ django_hello_world/hello/requestlogger.py
@@ -1,3 +1,8 @@
+from django_hello_world.hello.models import RequestsStorage
+
 class RequestLogger():
     def process_request(self, request):
+        new_request = RequestsStorage(body = request.path, method = request.method)
+        new_request.save(force_insert=True)
+        #new_request.time will be set to now by default
         return None
