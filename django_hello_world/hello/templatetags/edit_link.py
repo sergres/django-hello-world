0df73be5cecc540ff24fa76e59e8f5278f54d2f0
sergres <sergres@gmail.com>
2012-09-17 23:30:35 +0300

    ticket:8 lint - tab to spaces replacement

--- django_hello_world/hello/templatetags/edit_link.py
+++ django_hello_world/hello/templatetags/edit_link.py
@@ -8,8 +8,8 @@ register = template.Library()
 @register.simple_tag
 def edit_link(object):
     #return value.lower(
-    url = reverse('admin:%s_%s_change' % 
-    	(object._meta.app_label, object._meta.module_name),  args=[object.id])
+    url = reverse('admin:%s_%s_change' %
+        (object._meta.app_label, object._meta.module_name), args=[object.id])
     #url = "admin"
-    print object.__class__.__name__
+    #print object.__class__.__name__
     return u'<a href="%s">Edit %s</a>' % (url,  object.__class__.__name__)
