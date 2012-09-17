fe9fbd0d7e993e4dab3fb9d3d58cb80af614d592
sergres <sergres@gmail.com>
2012-09-17 23:25:48 +0300

    ticket:8 implemented tag - i have stackoverflov used for this

--- django_hello_world/hello/templatetags/edit_link.py
+++ django_hello_world/hello/templatetags/edit_link.py
@@ -1,8 +1,15 @@
 from django import template
+from django.core.urlresolvers import reverse
+
 
 register = template.Library()
 
 
 @register.simple_tag
-def edit_link(value):
-    return value.lower()
+def edit_link(object):
+    #return value.lower(
+    url = reverse('admin:%s_%s_change' % 
+    	(object._meta.app_label, object._meta.module_name),  args=[object.id])
+    #url = "admin"
+    print object.__class__.__name__
+    return u'<a href="%s">Edit %s</a>' % (url,  object.__class__.__name__)
