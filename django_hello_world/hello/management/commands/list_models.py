a5e68956180a6894f639072fdf4fa57cfbd8e48b
sergres <sergres@gmail.com>
2012-09-18 14:52:26 +0300

    ticket:9 implemented
    http://stackoverflow.com/questions/1125107/django-how-can-i-find-a-list-
    of-models-that-the-orm-knows

--- django_hello_world/hello/management/commands/list_models.py
+++ django_hello_world/hello/management/commands/list_models.py
@@ -7,5 +7,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('models: M1, M2, M3\n')
+        from django.contrib.contenttypes.models import ContentType
+
+        for ct in ContentType.objects.all():
+            m = ct.model_class()
+            print "%s.%s\t%d" % (m.__module__, m.__name__, m._default_manager.count())
 
 
