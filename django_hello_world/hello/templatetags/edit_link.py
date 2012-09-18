cf74979f7dc96a00dbe65e88e5ef1236aba4478e
sergres <sergres@gmail.com>
2012-09-18 11:08:54 +0300

    ticket:8 return nothing, in case we got a string as param

--- django_hello_world/hello/templatetags/edit_link.py
+++ django_hello_world/hello/templatetags/edit_link.py
@@ -7,9 +7,10 @@ register = template.Library()
 
 @register.simple_tag
 def edit_link(object):
-    #return value.lower(
+    # return nothing, in case we got a string
+    if object is "":
+        return u''
+
     url = reverse('admin:%s_%s_change' %
         (object._meta.app_label, object._meta.module_name), args=[object.id])
-    #url = "admin"
-    #print object.__class__.__name__
     return u'<a href="%s">Edit %s</a>' % (url,  object.__class__.__name__)
